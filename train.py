78db8d0a78cff72f2b926935adf2d2dfa67096e6
liam <liam@gust.show>
2021-12-30 07:49:51 +0100

    change exp name

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ from dropnprune import Pruner
 seed_everything(7)
 
 
-EXP_NAME = "prune0.4-cosineWarm5-sqrtInTrend-fixDrop"
+EXP_NAME = "prune0.4-cosineWarm5-moreFixes"
 PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
 PATH_DATASETS = "/home/liam/woven-cifar10-challenge-master/data"
 BATCH_SIZE = 128
