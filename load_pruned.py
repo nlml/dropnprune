a55ed1b253d0f40ffd0e5017bcd354380d3a7ddc
liam <liam@gust.show>
2022-01-01 10:47:08 +0100

    prune0.4-cosineWarm50fix-lpow1mult100-drop0.05

--- load_pruned.py
+++ load_pruned.py
@@ -23,7 +23,8 @@ def complexity(net):
 # ckpt_path = "lightning_logs/prune0.4-ratioScore-cosineWarm10-lpow1mult1/version_0/checkpoints/epoch=199-step=77999.ckpt"
 # ckpt_path = "lightning_logs/prune0.4-ratioScore-c osineWarm10-lpow1mult1-keep1fix/version_0/checkpoints/epoch=199-step=77999.ckpt"
 # ckpt_path = "lightning_logs/prune0.4-cosineWarm50-lpow1/version_0/checkpoints/epoch=199-step=77999.ckpt"
-ckpt_path = "lightning_logs/prune0.3-cosineWarm50-lpow1mult100/version_0/checkpoints/epoch=199-step=77999.ckpt"
+# ckpt_path = "lightning_logs/prune0.3-cosineWarm50-lpow1mult100/version_0/checkpoints/epoch=199-step=77999.ckpt"
+ckpt_path = "lightning_logs/prune0.5-cosineWarm50fix-lpow1mult100-nonRatio/version_0/checkpoints/epoch=199-step=77999.ckpt"
 ckpt = torch.load(ckpt_path)
 state_dict = ckpt["state_dict"]
 
