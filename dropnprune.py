c24fd8cbacbd24fdce8475a3c09de7afc5494e8b
liam <liam@gust.show>
2022-01-01 05:41:12 +0100

    prune0.4-cosineWarm100-lpow1

--- dropnprune.py
+++ dropnprune.py
@@ -147,7 +147,7 @@ class Pruner:
         pruning_freq: Optional[int] = None,
         prune_on_batch_idx: Optional[int] = 0,
         pct_to_prune: float = 0.4,
-        sched_cfg: dict = {"type": "cosine", "warmup": 50, "finish": 50},
+        sched_cfg: dict = {"type": "cosine", "warmup": 100, "finish": 0},
         detrending_on: bool = False,
         dropout_ratio_mode: bool = True,
         lambda_multiplier: float = 1,
