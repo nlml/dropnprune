f4775ed428473f78e2b2e873e7f20298a2053fcb
liam <liam@gust.show>
2022-01-03 09:54:23 +0100

    prune0.4-cosineWarm50fix-lmult0-drop0.01-every5-seed3

--- train.py
+++ train.py
@@ -14,9 +14,9 @@ from torchmetrics.functional import accuracy
 from resnet import resnet32
 from dropnprune import Pruner
 
-seed_everything(2)
+seed_everything(3)
 
-EXP_NAME = "prune0.4-cosineWarm0fix-lmult0-drop0.01-every5-seed2"
+EXP_NAME = "prune0.4-cosineWarm50fix-lmult0-drop0.01-every5-seed3"
 PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
 PATH_DATASETS = "/home/liam/woven-cifar10-challenge-master/data"
 BATCH_SIZE = 128
