1546c8fb28fd191b458cb9f98b7b3373766752a9
liam <liam@gust.show>
2022-01-06 07:25:49 +0100

    prune0.4-warm0finish190-drop0.01-every5-ma50-seed3

--- dropnprune.py
+++ dropnprune.py
@@ -175,7 +175,7 @@ class Pruner:
         pruning_freq: Optional[int] = None,
         prune_on_batch_idx: Optional[int] = 0,
         pct_to_prune: float = 0.4,
-        sched_cfg: dict = {"type": "cosine", "warmup": 0, "finish": 180},
+        sched_cfg: dict = {"type": "cosine", "warmup": 0, "finish": 190},
         detrending_on: bool = True,
         dropout_ratio_mode: bool = False,
         lambda_multiplier: float = 0,
