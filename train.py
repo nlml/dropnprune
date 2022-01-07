479a141871740c21f325d8dd37207b990b180561
liam <liam@gust.show>
2022-01-07 08:11:10 +0100

    prune0.4-warm50finish0-drop0.01-every10-ma50-seed3

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ from dropnprune import Pruner
 
 seed_everything(3)
 
-EXP_NAME = "prune0.4-warm0finish0-drop0.01-every10-ma50-seed3"
+EXP_NAME = "prune0.4-warm50finish0-drop0.01-every10-ma50-seed3"
 PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
 PATH_DATASETS = "/home/liam/woven-cifar10-challenge-master/data"
 BATCH_SIZE = 128
