092e2612ff5fc846575cce891d91818010e76df6
liam <liam@gust.show>
2022-01-12 17:19:12 +0100

    prune0.4-drop0.01-every5-maNone-threshT2.0lim-lr0.05-seed3

--- dropnprune.py
+++ dropnprune.py
@@ -234,7 +234,7 @@ class Pruner:
         lambda_multiplier: float = 0,
         lambda_pow: float = 1,
         prune_every_epoch: Optional[int] = 5,
-        ma: Optional[int] = 50,
+        ma: Optional[int] = None,
         score_threshold: float = 2.0,
         div_se: bool = True,
     ):
